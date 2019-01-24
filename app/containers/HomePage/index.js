e84d4565cd0a8c7776df42ee286f361d247b1628
Cris Ryan Tan <crisryantan@gmail.com>
2019-01-24 22:11:09 +1100

    add initial skeleton for application

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,25 +1,33 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
-
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import { Row, Col } from 'antd';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 90px 60px;
+  background-color: #ededed;
+`;
+
+const SubWrapper = styled.div`
+  height: -webkit-fill-available;
+  background-color: #ffffff;
+`;
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <Wrapper>
+        <SubWrapper>
+          <Row type="flex">
+            <Col xs={{ span: 24, order: 2 }} sm={{ span: 12, order: 1 }}>
+              Left
+            </Col>
+            <Col xs={{ span: 24, order: 1 }} sm={{ span: 12, order: 2 }}>
+              Right
+            </Col>
+          </Row>
+        </SubWrapper>
+      </Wrapper>
     );
   }
 }
